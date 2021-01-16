160eac57018653bfe8b043c42a800549bef3f77a
Afrie Irham <itsafrie@gmail.com>
2021-01-17 02:13:51 +0800

    Add more button to dashboard ui

--- main.py
+++ main.py
@@ -27,13 +27,41 @@ def quit_dashboard():
     top.destroy()
 
 
+def takeoff():
+    print("Drone takeoff")
+    billy.takeoff()
+
+
+def reset_position():
+    print("Go to checkpoint 0")
+    billy.reset_position()
+
+
+def ready_to_land():
+    print("Return to charging base")
+    billy.ready_to_land()
+
+
+def land():
+    print("Drone landing")
+    billy.land()
+
+
 # Add dashboard button
-sweepButton = Button(top, height=2, width=20, text="Start Parameter Sweep", command=start_sweep)
+resetPositionButton = Button(top, height=2, width=20, text="Ready position", command=reset_position)
+takeoffButton = Button(top, height=2, width=20, text="Take Off", command=takeoff)
+landButton = Button(top, height=2, width=20, text="Land", command=land)
+sweepButton = Button(top, height=2, width=20, text="Run sweep", command=start_sweep)
 overrideButton = Button(top, height=2, width=20, text="Override", command=override_sweep)
+readyToLandButton = Button(top, height=2, width=20, text="Prepare to land", command=ready_to_land)
 quitButton = Button(top, height=2, width=20, text="Quit", command=quit_dashboard)
 
+resetPositionButton.pack(padx=10, pady=5)
+takeoffButton.pack(padx=10, pady=5)
+landButton.pack(padx=10, pady=5)
 sweepButton.pack(padx=10, pady=5)
 overrideButton.pack(padx=10, pady=5)
+readyToLandButton.pack(padx=10, pady=5)
 quitButton.pack(padx=10, pady=5)
 
 top.mainloop()
