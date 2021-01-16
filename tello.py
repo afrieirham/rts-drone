662c81674ed9b9d778b15c7326a18c71736332f0
Afrie Irham <itsafrie@gmail.com>
2021-01-17 00:27:07 +0800

    Add threading to parameter sweep function

--- tello.py
+++ tello.py
@@ -1,8 +1,24 @@
 # This code is adopted from https://learn.droneblocks.io/p/tello-drone-programming-with-python/
 # Import the necessary modules
+from pip._vendor.distlib.compat import raw_input
 import socket
+import sys
 import threading
 import time
+
+# Travel to/from starting checkpoint 0 from/to the charging base
+frombase = ["forward", 50, "ccw", 150]
+tobase = ["ccw", 150, "forward", 50]
+
+# Flight path to Checkpoint 1 to 5 and back to Checkpoint 0 sequentially
+checkpoint = [
+    [1, "cw", 90, "forward", 100],
+    [2, "ccw", 90, "forward", 80],
+    [3, "ccw", 90, "forward", 40],
+    [4, "ccw", 90, "forward", 40],
+    [5, "cw", 90, "forward", 60],
+    [0, "ccw", 90, "forward", 40]
+]
 
 
 class Tello():
@@ -26,8 +42,12 @@ class Tello():
         self.receiveThread.daemon = True
         self.receiveThread.start()
 
+        # Setup parameter_sweep thread
+        self.parameter_sweep_thread = threading.Thread(target=self.sweep)
+        self.parameter_sweep_lock = True
+
     # Send the message to Tello and allow for a delay in seconds
-    def send(self, message, delay):
+    def send(self, message, delay=1):
         # Try to send the message otherwise print the exception
         try:
             self.sock.sendto(message.encode(), self.tello_address)
@@ -52,4 +72,104 @@ class Tello():
                 print("Error receiving: " + str(e))
             break
 
+    def sweep(self):
+        # Put Tello into command mode
+        self.send("command", 3)
+        if not self.parameter_sweep_lock: return False
 
+        # Send the takeoff command
+        self.send("takeoff", 3)
+        if not self.parameter_sweep_lock: return False
+
+        # Start at checkpoint 1 and print destination
+        print("From the charging base to the starting checkpoint of sweep pattern.\n")
+
+        self.send(frombase[0] + " " + str(frombase[1]), 4)
+        if not self.parameter_sweep_lock: return False
+
+        self.send(frombase[2] + " " + str(frombase[3]), 4)
+        if not self.parameter_sweep_lock: return False
+
+        print("Current location: Checkpoint 0 " + "\n")
+
+        # Billy's flight path
+        for i in range(len(checkpoint)):
+            if i == len(checkpoint) - 1:
+                print("Returning to Checkpoint 0. \n")
+
+            self.send(checkpoint[i][1] + " " + str(checkpoint[i][2]), 4)
+            if not self.parameter_sweep_lock: return False
+            self.send(checkpoint[i][3] + " " + str(checkpoint[i][4]), 4)
+            if not self.parameter_sweep_lock: return False
+
+            print("Arrived at current location: Checkpoint " +
+                  str(checkpoint[i][0]) + "\n")
+            time.sleep(4)
+
+        # Reach back at Checkpoint 0
+        print("Complete sweep. Return to charging base.\n")
+        self.send(tobase[0] + " " + str(tobase[1]), 4)
+        if not self.parameter_sweep_lock: return False
+        self.send(tobase[2] + " " + str(tobase[3]), 4)
+        if not self.parameter_sweep_lock: return False
+
+        # Turn to original direction before land
+        print("Turn to original direction before land.\n")
+        self.send("cw 180", 4)
+        if not self.parameter_sweep_lock: return False
+
+        # Land
+        self.send("land", 3)
+        if not self.parameter_sweep_lock: return False
+
+    def close_socket(self):
+        # Close the socket
+        self.sock.close()
+
+    def run_sweep(self):
+        self.parameter_sweep_thread.daemon = True
+        self.parameter_sweep_thread.start()
+
+    def terminate_sweep(self):
+        print("Parameter sweep overridden")
+        self.parameter_sweep_lock = False
+
+
+billy = Tello()
+
+# Tell the user what to do
+print('Type in a Tello SDK command and press the enter key. Enter "quit" to exit this program.')
+
+# Loop infinitely waiting for commands or until the user types quit or ctrl-c
+while True:
+
+    try:
+        # Read keyboard input from the user
+        if sys.version_info > (3, 0):
+            # Python 3 compatibility
+            message = input('')
+        else:
+            # Python 2 compatibility
+            message = raw_input('')
+
+        # If user types quit then lets exit and close the socket
+        if 'quit' in message:
+            print("Program exited successfully")
+            billy.close_socket()
+            break
+
+        if 'sweep' in message:
+            print("Initiate parameter sweep")
+            billy.run_sweep()
+
+        if 'override' in message:
+            print("Pre planned route override")
+            billy.terminate_sweep()
+
+        # Send the command to Tello
+        billy.send(message)
+
+    # Handle ctrl-c case to quit and close the socket
+    except KeyboardInterrupt as e:
+        billy.close_socket()
+        break
