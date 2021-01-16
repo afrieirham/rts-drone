15539085a9e59cc6962f4ee6765b467fd2fec891
Afrie Irham <itsafrie@gmail.com>
2021-01-17 01:04:19 +0800

    Add tkinter as tello dashboard ui

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
-import sys
-
-from pip._vendor.distlib.compat import raw_input
+from tkinter import *
 
 from tello import Tello
 
+# Initialize tello
 billy = Tello()
 
-# Tell the user what to do
-print('Type in a Tello SDK command and press the enter key. Enter "quit" to exit this program.')
+# Setup tkinter window
+top = Tk()
+top.title('Tello Controller')
+top.geometry('600x400')
 
-# Loop infinitely waiting for commands or until the user types quit or ctrl-c
-while True:
 
-    try:
-        # Read keyboard input from the user
-        if sys.version_info > (3, 0):
-            # Python 3 compatibility
-            message = input('')
-        else:
-            # Python 2 compatibility
-            message = raw_input('')
+def start_sweep():
+    print("Initiate parameter sweep")
+    billy.run_sweep()
 
-        # If user types quit then lets exit and close the socket
-        if 'quit' in message:
-            print("Program exited successfully")
-            billy.close_socket()
-            break
 
-        # Run parameter sweep function
-        if 'sweep' in message:
-            print("Initiate parameter sweep")
-            billy.run_sweep()
+def override_sweep():
+    print("Parameter sweep overridden")
+    billy.terminate_sweep()
 
-        # Override parameter sweep
-        if 'override' in message:
-            print("Parameter sweep overridden")
-            billy.terminate_sweep()
 
-        # Send the command to Tello
-        billy.send(message)
+def quit_dashboard():
+    print("Program exited successfully")
+    billy.close_socket()
+    top.destroy()
 
-    # Handle ctrl-c case to quit and close the socket
-    except KeyboardInterrupt as e:
-        billy.close_socket()
-        break
+
+# Add dashboard button
+sweepButton = Button(top, height=2, width=20, text="Start Parameter Sweep", command=start_sweep)
+overrideButton = Button(top, height=2, width=20, text="Override", command=override_sweep)
+quitButton = Button(top, height=2, width=20, text="Quit", command=quit_dashboard)
+
+sweepButton.pack(padx=10, pady=5)
+overrideButton.pack(padx=10, pady=5)
+quitButton.pack(padx=10, pady=5)
+
+top.mainloop()
